8d5081bef3999397c9ba8a85fccb6e790e184936
onlyphantom <samuel@outlook.sg>
2020-03-29 17:32:15 +0700

    add cross-browser compatible selenium script

--- demo_selenium.py
+++ demo_selenium.py
@@ -2,22 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time 
 
+MAX = 5
+
 browser = webdriver.Safari()
 browser.get('https://xkcd.com')
 
-try:
-    # Works also:
-    # a = browser.find_element_by_id('comic').find_element_by_tag_name('img')
-    time.sleep(2)
-    browser.execute_script('window.scrollTo(0, document.getElementById("comic").scrollHeight);')
-    time.sleep(3)
-    a = browser.find_element_by_xpath('//*[@id="comic"]/img')
-    print("Image Source:", a.get_attribute('src'))
-    # TODO: save the source into a txt file
+with open('img.txt', 'w') as f:
 
-    prev = browser.find_element_by_link_text("< Prev")
-    # In other browsers: prev.click()
-    prev.send_keys(Keys.RETURN)
-    print("Navigated to previous entry.")
-except:
-    print('Unable to find comic image.')
+    for i in range(MAX):
+        try:
+            # Works also:
+            # a = browser.find_element_by_id('comic').find_element_by_tag_name('img')
+            time.sleep(1)
+            browser.execute_script('window.scrollTo(0, document.getElementById("comic").scrollHeight);')
+            time.sleep(3)
+            a = browser.find_element_by_xpath('//*[@id="comic"]/img')
+            src = a.get_attribute('src')
+            print("Image Source:", src)
+            f.write(src + '\n')
+
+            prev = browser.find_element_by_link_text("< Prev")
+            # In other browsers: prev.click()
+            prev.send_keys(Keys.RETURN)
+            print("Navigated to previous entry.")
+        except:
+            print('Unable to find comic image.')
